505e2abb406e8fef05ed7cd596a91e41112fc4af
Michael Neely <michael@michaelneely.net>
2023-10-25 10:48:19 -0400

    Cleaaning up tree

--- app.js
+++ app.js
@@ -5,3 +5,22 @@ function randomIntBetween(min, max) {
 }
 
 console.log(randomIntBetween(0, 250));
+
+function productDescription(strings, productName, productPrice) {
+
+    console.log(strings);
+    console.log(productName);
+    console.log(productPrice);
+    let priceCategory = 'cheaply priced';
+    if(productPrice > 20) {
+        priceCategory = 'fairly priced';
+    }
+return `${strings[0]}${productName}${strings[1]}${priceCategory}${strings[2]}`;
+}
+
+const prodName = 'JavaScript Course';
+const prodPrice = 29.99;
+
+const productOutput = productDescription`This product (${prodName}) is ${prodPrice}.`;
+
+console.log(productOutput);
